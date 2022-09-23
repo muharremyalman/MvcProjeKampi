db2b030600eb1a4d1c8cf49e10c4ee67191bff56
muharremyalman <yalmanmuharrem507@gmail.com>
2022-09-24 01:59:31 +0300

    yazarlar finish

--- BusinessLayer/Concrete/MessageManager.cs
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -23,16 +23,16 @@ namespace BusinessLayer.Concrete
             return _messageDal.Get(x => x.MessageID == id);
         }
 
-        public List<Message> GetListInbox()
+        public List<Message> GetListInbox(string p)
         {
             return _messageDal.List(x => x.ReceiverMail == 
-            "kahraman@gmail.com");
+            p);
         }
 
 
-        public List<Message> GetListSendbox()
+        public List<Message> GetListSendbox(string p)
         {
-            return _messageDal.List(x => x.SenderMail == "kahraman@gmail.com");
+            return _messageDal.List(x => x.SenderMail == p);
         }
 
         public void MessageAdd(Message message)
