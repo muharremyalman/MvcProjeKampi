f4afbfdffcd7e6a6f004d009af5ce90fad3cc0a8
muharremyalman <yalmanmuharrem507@gmail.com>
2022-10-01 20:52:01 +0300

    Google chart tamamlandı

--- MvcProjeKampi/Controllers/ChartController.cs
+++ MvcProjeKampi/Controllers/ChartController.cs
@@ -1,9 +1,11 @@
 ﻿using MvcProjeKampi.Models;
 using System;
 using System.Collections.Generic;
+using DataAccessLayer.Concrete;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace MvcProjeKampi.Controllers
 {
@@ -15,38 +17,128 @@ namespace MvcProjeKampi.Controllers
             return View();
         }
 
-        public ActionResult CategoryChart()
+        public ActionResult CategoryListPieChart()
         {
-            return Json(BlogList(), JsonRequestBehavior.AllowGet);
+            return View();
         }
-        public List<CategoryClass> BlogList()
+
+        public ActionResult HeadingListColumnChart()
         {
-            List<CategoryClass> ct = new List<CategoryClass>();
-            ct.Add(new CategoryClass()
-            {
-                
-                CategoryCount = 4
-
-            });
-            ct.Add(new CategoryClass()
-            {
-                
-                CategoryCount = 7
-            });
-
-            ct.Add(new CategoryClass()
-            {
-                
-                CategoryCount = 1
-            });
-
-            ct.Add(new CategoryClass()
-            {
-                
-                CategoryCount = 5
-            });
-            return ct;
-
+            return View();
         }
+
+        public ActionResult WriterListLineChart()
+        {
+            return View();
+        }
+
+        // Kategori icerisindeki Baslik Sayisi Listesi \\
+
+        //#region Mimarisiz Kategori icerisindeki Baslik Listesi Grafigi
+
+        //public ActionResult CategoryChart()
+        //{
+        //    return Json(BlogList(), JsonRequestBehavior.AllowGet); //Kategory islemleri icin kullanilan bir yazim komutu
+        //}
+
+        //public List<CategoryClass> BlogList()
+        //{
+        //    List<CategoryClass> categoryClasses = new List<CategoryClass>();
+        //    categoryClasses.Add(new CategoryClass()
+        //    {
+        //        CategoryName = "Yazılım",
+        //        CategoryCount = 8
+        //    });
+        //    categoryClasses.Add(new CategoryClass()
+        //    {
+        //        CategoryName = "Gezi",
+        //        CategoryCount = 4
+        //    });
+        //    categoryClasses.Add(new CategoryClass()
+        //    {
+        //        CategoryName = "Teknoloji",
+        //        CategoryCount = 7
+        //    });
+        //    categoryClasses.Add(new CategoryClass()
+        //    {
+        //        CategoryName = "Spor",
+        //        CategoryCount = 3
+        //    });
+        //    return categoryClasses;
+        //}
+
+        //#endregion
+
+        public ActionResult CategoryListChart()
+        {
+            return Json(CategoryList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<CategoryClass> CategoryList()
+        {
+            List<CategoryClass> categoryClasses = new List<CategoryClass>();
+            using (var context = new Context())
+            {
+                categoryClasses = context.Categories.Select(x => new CategoryClass
+                {
+                    CategoryName = x.CategoryId,
+                    CategoryCount = x.Headings.Count()
+                }).ToList();
+            }
+            return categoryClasses;
+        }
+
+        // Yazarlarin Baslik Sayisi Listesi \\
+
+        //#region Mimarili Yazar Baslik Sayisi Listesi Grafigi
+
+        //public ActionResult WriterListChart()
+        //{
+        //    return Json(WriterList(), JsonRequestBehavior.AllowGet);
+        //}
+
+        //public List<WriterClass> WriterList()
+        //{
+        //    List<WriterClass> writerClasses = new List<WriterClass>();
+        //    using (var context = new Context())
+        //    {
+        //        writerClasses = context.Writers.Select(x => new WriterClass
+        //        {
+        //            WriterName = x.WriterName,
+        //            WriterCount = x.Headings.Count()
+        //        }).ToList();
+        //    }
+        //    return writerClasses;
+        //}
+
+        //#endregion
+
+
+        // Basliklarin Icerik Sayisi Listesi \\
+
+       /* #region*/ /*Mimarili Baslik  Icerik Sayisi Listesi Grafigi*/
+
+        //public ActionResult HeadingListChart()
+        //{
+        //    return Json(HeadingList(), JsonRequestBehavior.AllowGet);
+        //}
+
+        //public List<HeadingClass> HeadingList()
+        //{
+        //    List<HeadingClass> headingClasses = new List<HeadingClass>();
+        //    using (var context = new Context())
+        //    {
+        //        headingClasses = context.Headings.Select(x => new HeadingClass
+        //        {
+        //            HeadingName = x.HeadingName,
+        //            HeadingCount = x.Contents.Count()
+        //        }).ToList();
+        //    }
+
+        //    return headingClasses;
+        //}
+
+        //#endregion
+
     }
 }
